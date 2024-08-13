4e5049cfda9e3580f1f7e6bba74e8e598353553e
bgiannis <bgiannis@ci.com>
2024-08-13 13:19:22 -0400

    Update streamlit_app.py
    import pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 # Write directly to the app
 # Need space after first occurrence of ":cup_with_straw: " otherwise you get a syntax error
@@ -20,8 +21,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients;'
