59d846a900f1a4cd7c5fd0e5b800c8612611e97e
bgiannis <bgiannis@ci.com>
2024-08-13 10:37:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,12 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
 
         # request to pull in fruityvice data
         # putting JSON into a Dataframe so it will display as table.
         # use_container_width --> means table will expand to fill space of page
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
